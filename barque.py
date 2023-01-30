9b2e96358659928168757c59f1aa9e6c20163f09
Youcef BALEH <youcefbaleh@MacBook-Pro-de-Youcef.local>
2023-01-30 09:25:56 +0100

    Debut du Mini Projet

---
+++ barque.py
@@ -0,0 +1,19 @@
+ 
+class barque:
+
+    
+    def __init__( self , nom , places , *arg):
+
+      print("I AM A NEW BARQUE !!")
+      self.nom=nom
+      self.places=places
+      for ele in arg :
+          if( ele =="C"):
+            self.contenu=self.contenu+"C"
+          if( ele=="M"):
+            self.contenu=self.contenu+"M"  
+     
+
+
+    def toString(self):
+        return "Je suis une Barque  mon Nom :"+self.nom+"\n"+"Mes places :"+str(self.places)+"\n "+self.contenu
---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+from barque import barque
+
+
+mybarque = barque("maBarque",3,"C","C","C","C","C","C","M","h","M")
+
+print(mybarque.toString())
