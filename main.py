55ccb8e354eedb13a1502f8024efd82c95134b20
Youcef BALEH <youcefbaleh@MacBook-Pro-de-Youcef.local>
2023-02-06 18:36:30 +0100

    juste sauv

--- main.py
+++ main.py
@@ -10,27 +10,51 @@ etatinit.describe()
 listeprio=ListePrio([etatinit],1)
 
 rangy=int((mybarque.places+1)*(mybarque.places+2)/2-1)#nombre de Moves
+
+etat=listeprio.remove()
+while (etat):
+    for move in range(rangy):
+        if(mybarque.possibleMove(etatinit,move)):
+            e=mybarque.changeDeCote(etatinit,3)
+            listeprio.add(e)
+            print("hello")
+            if e.finale:
+                break
+    etat=listeprio.remove()
+
+print(mybarque.possibleMove(etatinit,3))
 etatinit=mybarque.changeDeCote(etatinit,3)
 etatinit.describe()
+print(mybarque.possibleMove(etatinit,0))
 etatinit=mybarque.changeDeCote(etatinit,0)
 etatinit.describe()
+print(mybarque.possibleMove(etatinit,4))
 etatinit=mybarque.changeDeCote(etatinit,4)
 etatinit.describe()
+print(mybarque.possibleMove(etatinit,2))
 etatinit=mybarque.changeDeCote(etatinit,2)
 etatinit.describe()
+print(mybarque.possibleMove(etatinit,1))
 etatinit=mybarque.changeDeCote(etatinit,1)
 etatinit.describe()
+print(mybarque.possibleMove(etatinit,3))
 etatinit=mybarque.changeDeCote(etatinit,3)
 etatinit.describe()
+print(mybarque.possibleMove(etatinit,1))
 etatinit=mybarque.changeDeCote(etatinit,1)
 etatinit.describe()
+print(mybarque.possibleMove(etatinit,2))
 etatinit=mybarque.changeDeCote(etatinit,2)
 etatinit.describe()
+print(mybarque.possibleMove(etatinit,4))
 etatinit=mybarque.changeDeCote(etatinit,4)
 etatinit.describe()
+print(mybarque.possibleMove(etatinit,2))
 etatinit=mybarque.changeDeCote(etatinit,2)
 etatinit.describe()
+print(mybarque.possibleMove(etatinit,4))
 etatinit=mybarque.changeDeCote(etatinit,4)
 etatinit.describe()
-for i in mybarque.liste:
-    i.describe()
+
+print(etatinit.precedents)
+print(etatinit.finale)
