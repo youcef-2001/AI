a1e876c834a2c36bcaf9f754f15be23ab89119eb
Youcef BALEH <youcefbaleh@MacBook-Pro-de-Youcef.local>
2023-02-07 09:45:38 +0100

    confirmation du parcour prof

--- main.py
+++ main.py
@@ -11,16 +11,7 @@ listeprio=ListePrio([etatinit],1)
 
 rangy=int((mybarque.places+1)*(mybarque.places+2)/2-1)#nombre de Moves
 
-etat=listeprio.remove()
-while (etat):
-    for move in range(rangy):
-        if(mybarque.possibleMove(etatinit,move)):
-            e=mybarque.changeDeCote(etatinit,3)
-            listeprio.add(e)
-            print("hello")
-            if e.finale:
-                break
-    etat=listeprio.remove()
+
 
 print(mybarque.possibleMove(etatinit,3))
 etatinit=mybarque.changeDeCote(etatinit,3)
@@ -49,12 +40,13 @@ etatinit.describe()
 print(mybarque.possibleMove(etatinit,4))
 etatinit=mybarque.changeDeCote(etatinit,4)
 etatinit.describe()
-print(mybarque.possibleMove(etatinit,2))
-etatinit=mybarque.changeDeCote(etatinit,2)
+print(mybarque.possibleMove(etatinit,0))
+etatinit=mybarque.changeDeCote(etatinit,0)
 etatinit.describe()
-print(mybarque.possibleMove(etatinit,4))
-etatinit=mybarque.changeDeCote(etatinit,4)
+print(mybarque.possibleMove(etatinit,3))
+etatinit=mybarque.changeDeCote(etatinit,3)
 etatinit.describe()
 
 print(etatinit.precedents)
 print(etatinit.finale)
+[3, 0, 4, 2, 1, 3, 1, 2, 4, 0, 3]
